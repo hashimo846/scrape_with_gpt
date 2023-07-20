1a0bbf6db88e45b0cac82e1939fa6ec0706f640d
hashimo846 <ayaenum@gmail.com>
2023-07-20 10:59:03 +0900

    make .env.filepath

--- src/input_handler.py
+++ src/input_handler.py
@@ -1,9 +1,12 @@
+from google.oauth2 import service_account
 import json
-from google.oauth2.service_account import Credentials
-import gspread
+import os
 from typing import Dict, List
 
-def read_json(file_path:str='data/input.json') -> Dict:
+INPUT_PATH = os.getenv('INPUT_PATH')
+GOOGLE_CREDENTIAL_PATH = os.getenv('GOOGLE_CREDENTIAL_PATH')
+
+def read_json(file_path:str) -> Dict:
     with open(file_path, 'r') as f:
         return json.load(f)
 
@@ -11,7 +14,9 @@ def get_master(master_url:str) -> Dict:
     pass
 
 def main():
-    print(read_json())
+    credentials = service_account.Credentials.from_service_account_file(GOOGLE_CREDENTIAL_PATH)
+    print(credentials)
+    print(read_json(INPUT_PATH))
 
 if __name__ == '__main__':
     main()
