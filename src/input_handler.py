611bc0e98bf343b1a941930c5b74f6bfb0c48835
hashimo846 <ayaenum@gmail.com>
2023-07-20 10:24:43 +0900

    add google, gspread to requirements.txt add data/gss_credential.json

--- src/input_handler.py
+++ src/input_handler.py
@@ -1,13 +1,17 @@
 import json
-import os
+from google.oauth2.service_account import Credentials
+import gspread
+from typing import Dict, List
 
-def read_json(file_path='data/input.json'):
+def read_json(file_path:str='data/input.json') -> Dict:
     with open(file_path, 'r') as f:
         return json.load(f)
 
+def get_master(master_url:str) -> Dict:
+    pass
+
 def main():
     print(read_json())
-    # print(os.getcwd())
 
 if __name__ == '__main__':
     main()
